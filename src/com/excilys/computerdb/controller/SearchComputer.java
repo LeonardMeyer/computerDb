e16108548c0be7d4fd53dd606d49edfbd1641c34
LeonardMeyer <leonard.meyer@viacesi.fr>
2014-02-27 16:30:26 +0100

    Fix des dates et retour utilisateur sur date < 1970

---
+++ src/com/excilys/computerdb/controller/SearchComputer.java
@@ -0,0 +1,51 @@
+package com.excilys.computerdb.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.excilys.computerdb.business.domain.Computer;
+import com.excilys.computerdb.business.services.ComputerService;
+
+/**
+ * Servlet implementation class SearchComputer
+ */
+@WebServlet("/SearchComputer")
+public class SearchComputer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchComputer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nameToSearch = request.getParameter("search");
+		List<Computer> computers = ComputerService.getInstance().filterByName(nameToSearch.trim());
+		
+		request.setAttribute("computers", computers);
+		getServletContext().setAttribute("computerCount", computers.size());
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/dashboard.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
