38c3826c8462be5c4d8f910675332df8f8816f8f
LeonardMeyer <leonard.meyer@viacesi.fr>
2014-02-27 11:04:06 +0100

    Ajout pagination

---
+++ src/com/excilys/computerdb/controller/DeleteComputer.java
@@ -0,0 +1,53 @@
+package com.excilys.computerdb.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.excilys.computerdb.business.domain.Computer;
+import com.excilys.computerdb.business.services.ComputerService;
+
+/**
+ * Servlet implementation class DeleteComputer
+ */
+@WebServlet("/DeleteComputer")
+public class DeleteComputer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteComputer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int computerId = Integer.parseInt(request.getParameter("computerId"));
+		
+		boolean success = ComputerService.getInstance().delete(computerId);
+		
+		List<Computer> computers = ComputerService.getInstance().findAllInRange(0, 10);
+		request.setAttribute("computers", computers);
+		getServletContext().setAttribute("computerCount", computers.size());
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/dashboard.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
