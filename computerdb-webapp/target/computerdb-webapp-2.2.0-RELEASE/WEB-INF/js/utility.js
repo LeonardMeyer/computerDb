3b598a33188c507cbff9b60de2c25a4f9fd90866
LeonardMeyer <leonard.meyer@viacesi.fr>
2014-03-25 17:06:03 +0100

    Nettoyage des binaries

--- computerdb-webapp/target/computerdb-webapp-2.2.0-RELEASE/WEB-INF/js/utility.js
+++
@@ -1,6 +0,0 @@
-function deleteEnabled(){
-	
-	var id = document.getElementById("computerId").getAttribute("value");
-	document.getElementById("crudForm").setAttribute("action", "/computerdb-webapp/Computer/"+id+"/Delete"); 
-	document.getElementById("crudForm").submit();
-}
